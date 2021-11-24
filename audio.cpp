c73cdc69a71435b8ae88fb3c8373f0bdbf1f6511
Andrew <andrey_kuzmenko2907@mail.ru>
2021-11-24 22:01:25 +0300

    start audio

--- audio.cpp
+++ audio.cpp
@@ -1,45 +1,169 @@
+#include "RtAudio.h"
 #include <iostream>
-#include<queue>
-#include <RtAudio.h>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <functional>
+#include <algorithm>
+#include <thread>
+#include <unistd.h>
 
 
 
-template<typename T>
-class audio
+#define INITIAL_CAPASITY 44100
+#define SAMPLE_RATE 44100
+#define BUFFER_SIZE 512
+#define N_CNAHHELS 2
+#define FORMAT RTAUDIO_SINT16
+typedef signed short MY_TYPE;
+
+ #define SLEEP( milliseconds ) usleep( (unsigned long) (milliseconds * 1000.0) )
+
+
+class record
 {
 public:
-	T get_to_transfer();
-	void record_for_playback(T);
-	virtual void stream() = 0;
-protected:
-	std::queue<T> in;
-	std::queue<T> out;
+  record() {
+    std::cout << "record()"; 
+    parameters.deviceId = adc.getDefaultInputDevice();
+    parameters.nChannels = N_CNAHHELS;
+    butch_size = BUFFER_SIZE;
+  }
+  bool input(const char *puth);
+  void off() {status = false;}
+  
 private:
+  bool status;
+  static int recording_butch(void * /*outputBuffer*/, void *InputBuffer, unsigned int nBufferFrames,
+         double /*streamTime*/, RtAudioStreamStatus /*status*/, void *userData );
+  unsigned int butch_size;
+  inline static RtAudio::StreamParameters parameters;
+  RtAudio adc;
 };
 
-template <typename T>
-class Rtaudio:public audio<T>
+int record::recording_butch(void * /*outputBuffer*/, void *InputBuffer, unsigned int nBufferFrames,
+         double /*streamTime*/, RtAudioStreamStatus /*status*/, void *userData) {
+  FILE *file  = (FILE*)userData;
+  size_t count = fwrite(InputBuffer, sizeof(MY_TYPE) * parameters.nChannels, nBufferFrames, file);
+  if (count < nBufferFrames) {
+    return 1;
+  }
+  return 0;
+}
+
+bool record::input(const char *puth) {
+  FILE *file;
+  status = true;
+  std::cout << "input";
+  file = fopen(puth, "wb");
+  try {
+    adc.openStream( NULL, &parameters, FORMAT,
+                    SAMPLE_RATE, &butch_size, &recording_butch, (void*)&file);
+    adc.startStream();
+  }
+  catch ( RtAudioError& e ) {
+    e.printMessage();
+    exit( 0 );
+  }
+  while (true) {
+    if (!status) {
+      break;
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    std::cout << "while";
+    // SLEEP(100);
+  }
+  std::cout << "off while";
+  try {
+    // Stop the stream
+    adc.stopStream();
+  }
+  catch (RtAudioError& e) {
+    e.printMessage();
+  }
+  if ( adc.isStreamOpen() ) adc.closeStream();
+  fclose(file);
+  return true;
+}
+
+
+class play
 {
-public:
-Rtaudio() : channels(2), sampleRate(44100), bufferFrames(512), device(0) {}
-using audio<T>::in;
-using audio<T>::out;
-virtual void stream();
-
+  public:
+  play() {
+    parameters.deviceId = dac.getDefaultOutputDevice();
+    parameters.nChannels = N_CNAHHELS;
+    butch_size = BUFFER_SIZE;
+  }
+  bool output(const char *puth);
 private:
-void record_for_transfer(T);
-T get_to_playback();
-int inout(void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
-		   double streamTime, RtAudioStreamStatus status, void *data )
-size_t channels;
-size_t sampleRate;
-size_t bufferFrames;
-size_t device;
+  static int play_butch(void *OutputBuffer,  void */*InputBuffer*/, unsigned int nBufferFrames,
+         double /*streamTime*/, RtAudioStreamStatus /*status*/, void *userData );
+  unsigned int butch_size;
+  static inline RtAudio::StreamParameters parameters;
+  RtAudio dac;
 };
 
-
-
-int main() {
-
+int play::play_butch(void *OutputBuffer,  void */*InputBuffer*/, unsigned int nBufferFrames,
+         double /*streamTime*/, RtAudioStreamStatus /*status*/, void *userData ) {
+    FILE *file  = (FILE*)userData;
+    size_t count = fread(OutputBuffer, sizeof(MY_TYPE) * parameters.nChannels, nBufferFrames , file);
+    if (count < nBufferFrames) {
+        size_t bytes = (nBufferFrames - count) * parameters.nChannels * sizeof(MY_TYPE);
+        size_t start_byte = count * parameters.nChannels * sizeof(MY_TYPE);
+        memset((char*)(OutputBuffer) + start_byte, 0, bytes);
+        return 1;
+    }
     return 0;
+}
+
+bool play::output(const char *puth){
+  FILE *file;
+  file = fopen(puth, "rb");
+  try {
+    dac.openStream(&parameters, NULL,  FORMAT,
+                    SAMPLE_RATE, &butch_size, &play_butch, (void*)&file);
+    dac.startStream();
+  }
+  catch ( RtAudioError& e ) {
+    e.printMessage();
+    exit( 0 );
+  }
+
+  while ( true ) {
+    SLEEP( 100 ); // wake every 100 ms to check if we're done
+    if ( dac.isStreamRunning() == false ) break;
+  }
+  try {
+    // Stop the stream
+    dac.stopStream();
+  }
+  catch (RtAudioError& e) {
+    e.printMessage();
+  }
+  if ( dac.isStreamOpen() ) dac.closeStream();
+  return true;
+}
+
+
+
+int main() 
+{
+  record rec;
+  bool test;
+  std::thread t([&]()
+  {
+    // std::cout << "thread";
+    test = rec.input("test.raw");
+  });
+  rec.off();
+  t.join();
+
+  // rec.input("test.raw");
+  // int n;
+  // rec.off();
+
+  // play pl;
+  // pl.output("test.raw");
+  return 0;
 }
